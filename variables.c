01bc4db95247cc8877228b6fcfe879e872c2ba82
unknown <assoulaimaniwissal38@gmail.com>
2022-09-07 10:59:06 +0100

    first commit

---
+++ variables.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main () {
+	
+	char characterName [] = "wissal";
+	int characterAge     =  22;
+	
+	printf("bonjour, je m'appele %s.\n", characterName);
+	printf("j'ai %d ans.\n", characterAge);
+
+    char characterName2[] = "yassine";
+    characterAge    = 22;
+    
+    printf("mon ami s'appelle %s.\n",characterName2);
+    printf("%s a %d ans.\n", characterName2, characterAge);
+	
+	return 0;
+} 
