9c111ab67aaba5d484f0f1fe877aadcde98f7bc6
unknown <assoulaimaniwissal38@gmail.com>
2022-09-07 14:59:16 +0100

    6commit

--- getting user imput/les operateurs logiques.c
+++ getting user imput/les operateurs logiques.c
@@ -2,9 +2,23 @@
 
 int main(){
 	
-	int x= 100, y= 50, r;
-	r= (x<y)? 1: 0;
-	printf("%d", r);
+	
+	
+	int a, b, c;
+	int d, e, f;
+	printf("entrer num1: ");
+	scanf("%d", &a);
+	printf("entrer num2: ");
+	scanf("%d", &b);
+	c= (a<b)? 1: 0;
+	printf("%d", c);
+	
+	printf("entrer num1: ");
+	scanf("%d", &d);
+	printf("entrer num2: ");
+	scanf("%d", &e);
+	c= (d<e)? 1: 0;
+	printf("%d", f);
 	return 0;
 	
 }
