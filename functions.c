3839837dcfa24510d40edae273f49ef11236a15b
unknown <assoulaimaniwissal38@gmail.com>
2022-09-13 11:40:50 +0100

    7commit

--- functions.c
+++ functions.c
@@ -1,17 +1,24 @@
 #include<stdio.h>
 #include<stdlib.h>
-
-void numbers(int a){
-
-	printf("%d,",a);
-}
 int main(){
-	int num[10];
-	int a;
-	for (a=1; a<=10;a++){
-	numbers(a);
-	
-	}	return 0;
+	float T [318];
+	int i;
+	float S, M;
+for (i=0; i<318;i++){
+printf("donner la note de l'etudiant num %d ", i+1);
+scanf("%f", &T[i]);
+
+
 }
+
+
+S= 0;
+for(i=0; i<318; i++)
+	S = S + T[i];
+
+
+M= S/318;
+
+return 0;}
 
 
---
+++ functionss.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+void numbers(int i){
+	printf("%d,",i);
+	
+}
+int main(){
+	int num[100];
+	int i;
+	
+
+	
+//for (i=0; i<99;i++){
+			for (i=0; i<10;i++){
+		    printf("%d,",i);
+			numbers(i);
+	//	}
+		//	printf("\n");}
+
+	return 0;	
+	
+}
